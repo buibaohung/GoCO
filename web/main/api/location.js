bc10042ddefd8e5d03ab723c3958f04e2181f542
buibaohung <32baohung1997@gmail.com>
2021-01-23 12:47:51 +0700

    add docker-compose

--- web/main/api/location.js
+++ web/main/api/location.js
@@ -1,5 +1,5 @@
 import http from '../utils/http'
 
 export function getLocations(parentCode) {
-    return http.get(`http://localhost:3001/locations?parent_code=${parentCode}`)
+    return http.get(`/locations?parent_code=${parentCode}`)
 }
