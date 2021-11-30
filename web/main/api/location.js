ada4451037479240c4bad3ff633d35accbed42ec
“buibaohung” <32baohung1997@gmail.com>
2021-12-01 00:27:35 +0700

    change url

--- web/main/api/location.js
+++ web/main/api/location.js
@@ -1,5 +1,5 @@
 import http from '../utils/http'
 
 export function getLocations(parentCode) {
-    return http.get(`http://localhost:3006/locations?parent_code=${parentCode}`)
+    return http.get(`http://34.125.145.199:3006/locations?parent_code=${parentCode}`)
 }
